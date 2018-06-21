cb803e6a2de8dce3c876389293839ce8b4dac4e1
wxhnewStar <wxh_newStar@foxmail.com>
2018-06-22 00:33:04 +0800

    21号周四晚上这个时候终于搞定了测试是否能够异步处理登录服务器并且提示的功能

--- app/src/main/java/com/example/administrator/ftpclient/MainActivity.java
+++ app/src/main/java/com/example/administrator/ftpclient/MainActivity.java
@@ -4,10 +4,10 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Message;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.res.FontResourcesParserCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,9 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -28,33 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private  Button login;
     private  Button cancel;
     public static EditText host,port,name,pass;
-
-    public Handler handler = new Handler() {
-
-        @Override
-        public void publish(LogRecord logRecord) {
-
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public void close() throws SecurityException {
-
-        }
-
-        public void handleMessage(Message msg) {
-            String str = (String) msg.obj;
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("提示")
-                    .setMessage("抱歉，暂时无法连接Ftp服务器，请检查是否服务器是否开启以及信息是否无误！")
-                    .setPositiveButton("确定", null)
-                    .show();
-        };
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,26 +76,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setPositiveButton("确定", null)
                         .show();
             } else {
-                int port = 21;
                 //信息填写完整,那么就测试是否能够登录，如果能就跳转，不能就提示用户
-                try{
-                    port = Integer.parseInt(portStr);
-                }catch(NumberFormatException e) {
-                    new AlertDialog.Builder(this)
-                            .setTitle("警告")
-                            .setMessage("端口格式错误，应该为数字！")
-                            .setPositiveButton("确定", null)
-                            .show();
-                }finally {
-                    test t=new test(this,host,port,name,pass);
-                    new AlertDialog.Builder(this)
-                            .setTitle("提示")
-                            .setMessage("没事，我只是试一下行不行而已")
-                            .setPositiveButton("确定", null)
-                            .show();
-                    t.start();
-                }
-
+                    String can[]=new String[4];
+                    can[0]=host;
+                    can[1]=portStr;
+                    can[2]=name;
+                    can[3]=pass;
+                    LogTask task=new LogTask(this);
+                    task.execute(can);
             }
             //清空按钮
         }else if(v==findViewById(R.id.cancel_button)){
@@ -139,6 +97,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.LENGTH_LONG);
             tot.show();
         }
+     }
+
+//处理登录的异步函数
+     class LogTask extends AsyncTask<String,Void,Boolean>{
+         Context mContext;
+         public LogTask(Context ctx){
+             mContext=ctx;
+         }
+
+         protected Boolean doInBackground(String... Params){
+             String host=Params[0];
+             String portStr=Params[1];
+             String user=Params[2];
+             String pass=Params[3];
+             System.out.println(Params[1]);
+                 int port=21;
+                 System.out.println("进入异步函数了");
+                 port = Integer.parseInt(portStr);
+                 boolean flag=false;
+                 FTPManager manager= new FTPManager();
+                 try{
+                     flag=manager.connect(host,port,user,pass);
+                     System.out.println("Okokokokokkookoko连接到了");
+                 }catch (Exception e){
+                     System.out.println("开启连接出错");
+                     e.printStackTrace();
+                 }finally {
+                     return flag;
+                 }
+         }
+
+         protected void onPostExecute(Boolean flag){
+             System.out.println("处理结果函数内部");
+             if(flag){
+                 System.out.println("处理结果函数正确分支");
+                 Toast tot = Toast.makeText(
+                         mContext,
+                         "正在跳转",
+                         Toast.LENGTH_LONG);
+                 tot.show();
+
+             }else{
+                 System.out.println("处理结果函数错误分支");
+                 new AlertDialog.Builder(mContext)
+                         .setTitle("提示")
+                         .setMessage("抱歉，暂时无法连接Ftp服务器，请检查是否服务器是否开启以及信息是否无误！")
+                         .setPositiveButton("确定", null)
+                         .show();
+
+             }
+         }
      }
 }
 
